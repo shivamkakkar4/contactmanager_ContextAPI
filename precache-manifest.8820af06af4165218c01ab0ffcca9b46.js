6b00be3afad5c7e8d06927590dee42e2f6fa83df
shivam kakkar <shivamkakkar4@gmail.com>
2019-12-29 11:47:53 +0530

    Updates

---
+++ precache-manifest.8820af06af4165218c01ab0ffcca9b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d387ccd977097d6a5d2d78c18e4043e7",
+    "url": "/contactmanager_ContextAPI/index.html"
+  },
+  {
+    "revision": "11e3af0862aa7d793b30",
+    "url": "/contactmanager_ContextAPI/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "f6410e8ba9b3781f2e61",
+    "url": "/contactmanager_ContextAPI/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "11e3af0862aa7d793b30",
+    "url": "/contactmanager_ContextAPI/static/js/2.c78a7e3e.chunk.js"
+  },
+  {
+    "revision": "c8336e0bcbf8ea1670aafb15aa044bf7",
+    "url": "/contactmanager_ContextAPI/static/js/2.c78a7e3e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f6410e8ba9b3781f2e61",
+    "url": "/contactmanager_ContextAPI/static/js/main.7703b79a.chunk.js"
+  },
+  {
+    "revision": "e39fdbe69b301885ecc8",
+    "url": "/contactmanager_ContextAPI/static/js/runtime-main.a5ea2b00.js"
+  }
+]);
